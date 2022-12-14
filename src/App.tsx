aff8a0659bc44ca7fcb23c1dacf5d0513f85405a
Alan <alansferreira1984@gmail.com>
2022-12-14 21:08:51 +0000

    Update 4 files

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
-import { useState } from 'react';
+import { useState } from "react";
 import "./styles.css";
-import { Divider, Segment, Grid, Form, TextArea } from 'semantic-ui-react'
+import { Divider, Segment, Grid, Form, TextArea } from "semantic-ui-react";
 
-function shorten (str: string) {
+function shorten(str: string) {
   const regex = /(\w{1,4})[\w]{0,}/gm;
   const subst = `$1`;
   // The substituted value will be contained in the result variable
@@ -10,27 +10,31 @@ function shorten (str: string) {
 }
 
 export default function App() {
-  const [output, setOutput] = useState('')
+  const [output, setOutput] = useState("");
   const onChangeInput = (e: any) => {
-    const input  = e.target.value
-    const lines = input.replace(/\r\n/gm, '\n')
-      .replace(/\r/gm, '\n')
-      .split('\n')
+    const input = e.target.value;
+    const lines = input
+      .replace(/\r\n/gm, "\n")
+      .replace(/\r/gm, "\n")
+      .split("\n");
 
-    setOutput(lines.map((line: string) => shorten(line)).join('\n'))
-  }
+    setOutput(lines.map((line: string) => shorten(line)).join("\n"));
+  };
   return (
     <div className="App">
       <Segment>
-        <Grid columns={2} relaxed='very'>
+        <Grid columns={2} relaxed="very">
           <Grid.Column>
             <Form>
-              <TextArea onChange={onChangeInput} placeholder='Put your long term' />
+              <TextArea
+                onChange={onChangeInput}
+                placeholder="Put your long term"
+              />
             </Form>
           </Grid.Column>
           <Grid.Column>
             <Form>
-              <TextArea placeholder='Tell us more' value={output}/>
+              <TextArea placeholder="Tell us more" value={output} />
             </Form>
           </Grid.Column>
         </Grid>

b72fe8ac5daf777664bd0f9a75597753f2363d24
Alan <alansferreira1984@gmail.com>
2022-12-14 17:57:55 -0300

    Initial commit

---
+++ src/index.tsx
@@ -0,0 +1,14 @@
+import "semantic-ui-css/semantic.min.css";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+
+import App from "./App";
+
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement!);
+
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
